790cef6dbd98b4a39f22e2185762c705283ec1bc
kubarada <jakub.rd@seznam.cz>
2023-11-03 12:06:45 +0100

    final version + new script for generation of sound wav

--- generate_sound.py
+++ generate_sound.py
@@ -9,9 +9,9 @@ signal = np.random.uniform(-amplitude, amplitude, int(sample_rate * duration))
 left_channel = signal
 right_channel = signal
 
-silence = np.zeros(int(0.1 * sample_rate)) # simulované ticho
+silence = np.zeros(int(0.1 * sample_rate))
 
-channel1 = np.hstack((silence, left_channel, silence)) # první kanál je posunutý o 0.1 sekundy ticha
+channel1 = np.hstack((silence, left_channel, silence))
 channel2 = np.hstack((right_channel, silence, silence))
 
 stereo_signal = np.column_stack((channel1, channel2))
